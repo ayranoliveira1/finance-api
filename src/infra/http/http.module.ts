8e9a70d35701a822013304277b0181a874773950
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-04-16 14:26:11 -0300

    feat: add http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common'
+import { DataBaseModule } from '../database/database.module'
+import { CryptographyModule } from './cryptography/cryptography.module'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { RegisterUseCase } from '@/domain/application/use-case/register'
 
 @Module({
-  imports: [],
+  imports: [DataBaseModule, CryptographyModule],
+  controllers: [CreateAccountController],
+  providers: [RegisterUseCase],
 })
 export class HttpModule {}
