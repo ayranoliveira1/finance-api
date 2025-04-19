85ab2961bf2963f1e538236a90d36b6f023bdb2e
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-04-19 09:46:22 -0300

    feat: add create transaction use case test unit

---
+++ src/domain/application/use-case/transaction/create-transaction.spec.ts
@@ -0,0 +1,32 @@
+import { InMemoryTransactionRepository } from 'test/repositories/in-memory-transaction-repository'
+import { CreateTransactionUseCase } from './create-transaction'
+import { makeTransaction } from 'test/factories/make-transaction'
+
+let inMemoryTransactionRepository: InMemoryTransactionRepository
+let sut: CreateTransactionUseCase
+
+describe('Register Use Case', () => {
+  beforeEach(() => {
+    inMemoryTransactionRepository = new InMemoryTransactionRepository()
+    sut = new CreateTransactionUseCase(inMemoryTransactionRepository)
+  })
+
+  it('should be able to register a user', async () => {
+    const transaction = makeTransaction()
+
+    const result = await sut.execute({
+      name: transaction.name,
+      type: transaction.type,
+      category: transaction.category,
+      paymentMethod: transaction.paymentMethod,
+      amount: transaction.amount,
+      userId: transaction.userId.toString(),
+      date: transaction.date,
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      transaction: inMemoryTransactionRepository.items[0],
+    })
+  })
+})
