8bc63631b8513eea32e255beb17805ad0ebfa467
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-04-18 21:25:20 -0300

    feat: add in memory transaction repository

---
+++ test/repositories/in-memory-transaction-repository.ts
@@ -0,0 +1,26 @@
+import { TransactionRepository } from '@/domain/application/repositories/transaction-repository'
+import { Transaction } from '@/domain/enterprise/entities/transaction'
+
+export class InMemoryTransactionRepository implements TransactionRepository {
+  public items: Transaction[] = []
+
+  async findById(id: string): Promise<Transaction | null> {
+    const transaction = this.items.find((item) => item.id.toString() === id)
+
+    if (!transaction) {
+      return null
+    }
+
+    return transaction
+  }
+
+  async create(transaction: Transaction): Promise<void> {
+    this.items.push(transaction)
+  }
+
+  async delete(transaction: Transaction): Promise<void> {
+    const index = this.items.findIndex((item) => item.id === transaction.id)
+
+    this.items.splice(index, 1)
+  }
+}
