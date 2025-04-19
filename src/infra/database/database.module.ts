125c001af7bce562f6fad2bfe3eda25ad5a81714
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-04-19 14:55:21 -0300

    feat: add create transaction e2e test

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { UserRepository } from '@/domain/application/repositories/user-repository'
 import { PrimsaUserRepository } from './prisma/repositories/prisma-user-repository'
+import { TransactionRepository } from '@/domain/application/repositories/transaction-repository'
+import { PrismaTransactionRepository } from './prisma/repositories/prisma-transction-repository'
 
 @Module({
   providers: [
@@ -10,7 +12,11 @@ import { PrimsaUserRepository } from './prisma/repositories/prisma-user-reposito
       provide: UserRepository,
       useClass: PrimsaUserRepository,
     },
+    {
+      provide: TransactionRepository,
+      useClass: PrismaTransactionRepository,
+    },
   ],
-  exports: [PrismaService, UserRepository],
+  exports: [PrismaService, UserRepository, TransactionRepository],
 })
 export class DataBaseModule {}
---
+++ src/infra/http/controllers/create-transaction.controller.e2e-spec.ts
@@ -0,0 +1,51 @@
+import { AppModule } from '@/infra/app.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+
+describe('Authenticate (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get<PrismaService>(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /transactions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        password: await hash('12345678', 8),
+      },
+    })
+
+    const user = await request(app.getHttpServer()).post('/auth/login').send({
+      email: 'johndoe@gmail.com',
+      password: '12345678',
+    })
+
+    const response = await request(app.getHttpServer())
+      .post('/transactions')
+      .send({
+        name: 'Transaction 1',
+        amount: 100,
+        category: 'FOOD',
+        type: 'EXPENSE',
+        paymentMethod: 'CREDIT_CARD',
+      })
+      .set('Authorization', `Bearer ${user.body.token}`)
+
+    expect(response.statusCode).toBe(201)
+  })
+})
