2d5ef7e28ab7d65f33e0de8ddebac284515055f7
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-04-19 09:44:58 -0300

    feat: add type enums

---
+++ src/core/@types/enums.ts
@@ -0,0 +1,27 @@
+export enum TransactionType {
+  DEPOSIT,
+  EXPENSE,
+  INVESTMENT,
+}
+
+export enum TransactionCategory {
+  HOUSING,
+  TRANSPORTATION,
+  FOOD,
+  ENTERTAINMENT,
+  HEALTH,
+  UTILITY,
+  SALARY,
+  EDUCATION,
+  OTHER,
+}
+
+export enum TransactionPaymentMethod {
+  CREDIT_CARD,
+  DEBIT_CARD,
+  BANK_TRANSFER,
+  BANK_SLIP,
+  CASH,
+  PIX,
+  OTHER,
+}
