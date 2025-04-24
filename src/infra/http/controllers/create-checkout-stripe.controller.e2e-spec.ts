4e3573dc39c387602b1d34345f42fa387374cc8b
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-04-24 20:45:33 -0300

    feat: add create checkout stripe controller test e2e

---
+++ src/infra/http/controllers/create-checkout-stripe.controller.e2e-spec.ts
@@ -0,0 +1,46 @@
+import { AppModule } from '@/infra/app.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+
+describe('Create Checkout stripe (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get<PrismaService>(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /checkout', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        password: await hash('12345678', 8),
+      },
+    })
+
+    const user = await request(app.getHttpServer()).post('/auth/login').send({
+      email: 'johndoe@gmail.com',
+      password: '12345678',
+    })
+
+    const response = await request(app.getHttpServer())
+      .post('/checkout')
+      .send()
+      .set('Authorization', `Bearer ${user.body.token}`)
+
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toHaveProperty('checkout_url')
+  })
+})
