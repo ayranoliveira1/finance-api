5d2187425f6f6c2fbc685c52a98a1dbf028af03d
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-04-24 20:40:18 -0300

    feat: add create checkout stripe use case test unit

---
+++ src/domain/application/use-case/payment/create-checkout-stripe.spec.ts
@@ -0,0 +1,60 @@
+import { FakerStripeMethods } from 'test/payment/faker-stripe'
+import { InMemoryUserRepository } from 'test/repositories/in-memory-user-repository'
+import { CreateCheckoutStripeUseCase } from './create-checkout-stripe'
+import { makeUser } from 'test/factories/make-user'
+import { UserAlreadyHasThePlanError } from '../errors/user-already-has-the-plan-error'
+
+let fakerStripeMethod: FakerStripeMethods
+let inMemoryUserRepository: InMemoryUserRepository
+let sut: CreateCheckoutStripeUseCase
+
+describe('CreateCheckoutStripeUseCase', () => {
+  beforeEach(() => {
+    fakerStripeMethod = new FakerStripeMethods()
+    inMemoryUserRepository = new InMemoryUserRepository()
+    sut = new CreateCheckoutStripeUseCase(
+      inMemoryUserRepository,
+      fakerStripeMethod,
+    )
+  })
+
+  it('should be able to create a checkout stripe session', async () => {
+    const user = makeUser()
+
+    await inMemoryUserRepository.create(user)
+
+    const result = await sut.execute({
+      userId: user.id.toString(),
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      checkout_url: `https://fake-stripe.com/${user.id.toString()}/${user.email}`,
+    })
+  })
+
+  it('should not be able to create a checkout stripe session if user does not exist', async () => {
+    const user = makeUser()
+
+    const result = await sut.execute({
+      userId: user.id.toString(),
+    })
+
+    expect(result.isLeft()).toBe(true)
+  })
+
+  it('should not be able to create a checkout stripe session if user is not a USER', async () => {
+    const user = makeUser({
+      role: 'ADMIN',
+    })
+
+    await inMemoryUserRepository.create(user)
+
+    const result = await sut.execute({
+      userId: user.id.toString(),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(UserAlreadyHasThePlanError)
+  })
+})
