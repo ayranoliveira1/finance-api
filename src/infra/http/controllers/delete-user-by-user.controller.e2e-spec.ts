0ee3127bbff01e8fe67f72d1b23083299ade1dba
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-04-27 20:34:49 -0300

    feat: add delete user by user  controller test e2e

---
+++ src/infra/http/controllers/delete-user-by-user.controller.e2e-spec.ts
@@ -0,0 +1,47 @@
+import { AppModule } from '@/infra/app.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+
+describe('Delete User (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get<PrismaService>(PrismaService)
+
+    await app.init()
+  })
+
+  test('[Delete] /users', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        password: await hash('12345678', 8),
+      },
+    })
+
+    const user = await request(app.getHttpServer()).post('/auth/login').send({
+      email: 'johndoe@gmail.com',
+      password: '12345678',
+    })
+
+    const response = await request(app.getHttpServer())
+      .delete('/user')
+      .set('Authorization', `Bearer ${user.body.token}`)
+      .send({
+        password: '12345678',
+      })
+
+    expect(response.statusCode).toBe(204)
+  })
+})
