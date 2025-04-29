d20d50a93c546d22cdb30d63a094146f957e0eb3
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-04-29 11:46:37 -0300

    feat: add get user controller test e2e

---
+++ src/infra/http/controllers/user/get-user-controller.e2e-spec.ts
@@ -0,0 +1,52 @@
+import { AppModule } from '@/infra/app.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+
+describe('Get User (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get<PrismaService>(PrismaService)
+
+    await app.init()
+  })
+
+  test('[GET] /auth/me', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        password: await hash('12345678', 8),
+      },
+    })
+
+    const login = await request(app.getHttpServer()).post('/auth/login').send({
+      email: 'johndoe@gmail.com',
+      password: '12345678',
+    })
+
+    const response = await request(app.getHttpServer())
+      .get('/auth/me')
+      .set('Authorization', `Bearer ${login.body.token}`)
+
+    expect(response.statusCode).toBe(200)
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        user: expect.objectContaining({
+          id: expect.any(String),
+        }),
+      }),
+    )
+  })
+})
