e6267d1fbf1c65c0a28ea297f6d6e90c5db70cbc
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-06-10 16:39:01 -0300

    feat: add prisma session mapper

---
+++ src/infra/database/prisma/mappers/prisma-session-mapper.ts
@@ -0,0 +1,31 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Session } from '@/domain/enterprise/entities/session'
+import { Prisma, Session as SessionPrisma } from '@prisma/client'
+
+export class PrismaSessionMapper {
+  static toDomain(raw: SessionPrisma): Session {
+    return Session.create(
+      {
+        ip: raw.ip,
+        browser: raw.browser,
+        city: raw.city,
+        region: raw.region,
+        country: raw.country,
+        userId: raw.userId,
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+
+  static toPrisma(domain: Session): Prisma.SessionUncheckedCreateInput {
+    return {
+      id: domain.id.toString(),
+      ip: domain.ip,
+      browser: domain.browser,
+      city: domain.city,
+      region: domain.region,
+      country: domain.country,
+      userId: domain.userId,
+    }
+  }
+}
