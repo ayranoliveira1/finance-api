c66b715c4000b8ca5e2601e9a3540c9c643d964c
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-06-10 15:50:35 -0300

    feat: add create session use case test unit

---
+++ src/domain/application/use-case/user/create-session.spec.ts
@@ -0,0 +1,49 @@
+import { FakerLocation } from 'test/location/faker-location'
+import { InMemorySessionRepository } from 'test/repositories/in-memory-session-repository'
+import { CreateSessionUseCase } from './create-session'
+import { makeSession } from 'test/factories/make-session'
+import { ResourceNotFoundError } from '@/core/@types/errors/resource-not-found-error'
+
+let fakerLocation: FakerLocation
+let inMemorySessionRepository: InMemorySessionRepository
+let sut: CreateSessionUseCase
+
+describe('CreateSessionUseCase', () => {
+  beforeEach(() => {
+    fakerLocation = new FakerLocation()
+    inMemorySessionRepository = new InMemorySessionRepository()
+    sut = new CreateSessionUseCase(inMemorySessionRepository, fakerLocation)
+  })
+
+  it('should be able to create a session', async () => {
+    const session = makeSession()
+
+    const result = await sut.execute({
+      ip: session.ip,
+      browser: session.browser,
+      userId: session.userId.toString(),
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      session: inMemorySessionRepository.items[0],
+    })
+  })
+
+  it('should not be able to create a session if the location is not found', async () => {
+    const session = makeSession({
+      ip: '',
+      browser: 'Chrome',
+      userId: 'user-1',
+    })
+
+    const result = await sut.execute({
+      ip: session.ip,
+      browser: session.browser,
+      userId: session.userId.toString(),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
+  })
+})
