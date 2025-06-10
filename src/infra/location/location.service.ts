b53c069b570fae3a2663c64a2cfd548821e59678
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-06-10 16:39:54 -0300

    feat: add location service

---
+++ src/infra/location/location.service.ts
@@ -0,0 +1,21 @@
+import { LocationMethods } from '@/domain/application/location/location'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class LocationService implements LocationMethods {
+  async getLocationByIp(ip: string) {
+    const response = await fetch(`http://ip-api.com/json/${ip}`)
+
+    if (!response.ok) {
+      return null
+    }
+
+    const result = await response.json()
+
+    return {
+      country: result.country ?? 'unknown',
+      city: result.city ?? 'unknown',
+      region: result.region ?? 'unknown',
+    }
+  }
+}
