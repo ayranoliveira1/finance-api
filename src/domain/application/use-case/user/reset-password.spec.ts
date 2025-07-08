d927b1873ee67fd3a1b5000298e3b1f04c6d606b
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-07-08 17:51:16 -0300

    feat: add reset passwoord use case test unit

---
+++ src/domain/application/use-case/user/reset-password.spec.ts
@@ -0,0 +1,135 @@
+import { FakerHasher } from 'test/cryptography/faker-hasher'
+import { InMemoryUserRepository } from 'test/repositories/in-memory-user-repository'
+import { ResetPasswordUseCase } from './reset-password'
+import { makeUser } from 'test/factories/make-user'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+import { VerificationCodeExpiredError } from '@/core/@types/errors/verification-code-expired-error'
+import { InvalidCodeError } from '@/core/@types/errors/invalid-code-error'
+
+let inMemoryUserRepository: InMemoryUserRepository
+let fakerHasher: FakerHasher
+let sut: ResetPasswordUseCase
+
+describe('Reset Password Use Case', () => {
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository()
+    fakerHasher = new FakerHasher()
+    sut = new ResetPasswordUseCase(inMemoryUserRepository, fakerHasher)
+  })
+
+  it('should reset the user password successfully', async () => {
+    const user = makeUser({
+      verificationCode: '123456',
+      codeExpiresAt: new Date(Date.now() + 1000 * 60 * 10),
+    })
+
+    await inMemoryUserRepository.create(user)
+
+    const result = await sut.execute({
+      code: user.verificationCode!,
+      email: user.email,
+      newPassword: 'new_password',
+      confirmNewPassword: 'new_password',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      Message: 'Password reset successfully',
+    })
+  })
+
+  it('should reset the user password with a new hashed password', async () => {
+    const user = makeUser({
+      verificationCode: '123456',
+      codeExpiresAt: new Date(Date.now() + 1000 * 60 * 10),
+    })
+    await inMemoryUserRepository.create(user)
+
+    const result = await sut.execute({
+      code: user.verificationCode!,
+      email: user.email,
+      newPassword: 'new_password',
+      confirmNewPassword: 'new_password',
+    })
+
+    const hashedPassword = await fakerHasher.hash('new_password')
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryUserRepository.items[0].password).toBe(hashedPassword)
+  })
+
+  it('should return an error if the user does not exist', async () => {
+    const user = makeUser({
+      verificationCode: '123456',
+      codeExpiresAt: new Date(Date.now() + 1000 * 60 * 10),
+    })
+
+    await inMemoryUserRepository.create(user)
+
+    const result = await sut.execute({
+      code: user.verificationCode!,
+      email: 'non_existent_email@example.com',
+      newPassword: 'new_password',
+      confirmNewPassword: 'new_password',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('should return an error if the verification code is expired', async () => {
+    const user = makeUser({
+      verificationCode: '123456',
+      codeExpiresAt: new Date(Date.now() - 1000 * 60 * 10),
+    })
+
+    await inMemoryUserRepository.create(user)
+
+    const result = await sut.execute({
+      code: user.verificationCode!,
+      email: user.email,
+      newPassword: 'new_password',
+      confirmNewPassword: 'new_password',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(VerificationCodeExpiredError)
+  })
+
+  it('should return an error if the verification code is invalid', async () => {
+    const user = makeUser({
+      verificationCode: '123456',
+      codeExpiresAt: new Date(Date.now() + 1000 * 60 * 10),
+    })
+
+    await inMemoryUserRepository.create(user)
+
+    const result = await sut.execute({
+      code: 'invalid_code',
+      email: user.email,
+      newPassword: 'new_password',
+      confirmNewPassword: 'new_password',
+    })
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(InvalidCodeError)
+  })
+
+  it('should return an error if the new password and confirm password do not match', async () => {
+    const user = makeUser({
+      verificationCode: '123456',
+      codeExpiresAt: new Date(Date.now() + 1000 * 60 * 10),
+    })
+
+    await inMemoryUserRepository.create(user)
+
+    const result = await sut.execute({
+      code: user.verificationCode!,
+      email: user.email,
+      newPassword: 'new_password',
+      confirmNewPassword: 'different_password',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(InvalidCredentialsError)
+  })
+})
