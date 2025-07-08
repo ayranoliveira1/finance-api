2cf222c25115c35f0f2415b50ebdb04d6aed40bd
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-07-08 17:10:32 -0300

    feat: add send email reset password controller test e2e

---
+++ src/infra/http/controllers/mail/send-email-reset-password.controller.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { AppModule } from '@/infra/app.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+
+describe('Send Email Reset Password (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get<PrismaService>(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /mail/send-email-reset-password', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        password: await hash('12345678', 8),
+      },
+    })
+
+    const response = await request(app.getHttpServer())
+      .post('/mail/send-email-reset-password')
+      .send({
+        email: 'johndoe@gmail.com',
+      })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
