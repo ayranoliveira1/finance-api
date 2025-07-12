485413dfa2a94dc20ddf3a124bc37158882cb65d
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-07-12 18:32:18 -0300

    feat: add method find by verification code in user repositories

--- src/domain/application/repositories/user-repository.ts
+++ src/domain/application/repositories/user-repository.ts
@@ -3,6 +3,7 @@ import { User } from '@/domain/enterprise/entities/user'
 export abstract class UserRepository {
   abstract findByEmail(email: string): Promise<User | null>
   abstract findById(id: string): Promise<User | null>
+  abstract findByVerificationCode(code: string): Promise<User | null>
   abstract create(user: User): Promise<void>
   abstract delete(user: User): Promise<void>
   abstract updatePlan(userID: string, plan: string): Promise<void>
